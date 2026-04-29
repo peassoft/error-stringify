f41cdf2cb74e257eb26167b5f1e407ebb09bb1ac
Alexey Gorokhov <alexey.gorokhov@mail.ru>
2026-04-29 11:11:58 +0300

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import mnrConfig from '@memnrev/eslint-v9-config';
+
+export default [
+  {
+    ignores: [
+      'dist',
+    ],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  ...mnrConfig.configs.react,
+];
